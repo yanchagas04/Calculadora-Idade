a701423a14d27edcc6d074f04d00abfe182a38c5
Yan Chagas <yanchagas2004@gmail.com>
2025-03-25 08:42:01 -0300

    Init

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <div className='w-screen h-screen bg-slate-900 flex justify-center items-center'>
+        <h1 className='text-slate-50 text-2xl'>Calculadora Idade</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
